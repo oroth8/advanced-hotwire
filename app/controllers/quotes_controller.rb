e7cc0767600186c10d34dfd0da2491fe8bb72e22
Owen Roth <rothowen27@gmail.com>
2023-01-24 20:23:40 -0700

    added flash message turbo functionality

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -17,7 +17,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
       render :new
@@ -28,20 +28,23 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: 'Quote was successfully updated.'
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @quote.destroy
+  @quote.destroy
 
-    respond_to do |format|
-      format.html { redirect_to quotes_path, notice: 'Quote was successfully destroyed.' }
-      format.turbo_stream
-    end
+  respond_to do |format|
+    format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+    format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed." }
   end
+end
 
   private
 
